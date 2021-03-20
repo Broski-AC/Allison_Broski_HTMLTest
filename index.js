2d7e639d4ff67d6b253a4b76b7f3e61cf2f15a4c
Allison Broski <acbroski@oakland.edu>
2021-03-20 14:36:24 +0000

    Tested Javascript

---
+++ index.js
@@ -0,0 +1,16 @@
+// We need to add certain tags to the HTML to get user input
+
+// Get input from HTML file into JS file - linking JS and HTML together
+
+// Put the rules of FizzBuzz in our JS file
+// If input is divisible by 3, print 'Fizz'
+// If input is divisible by 5, print 'Buzz'
+// If input is divisible by 3 and 5, print 'FizzBuzz'
+
+
+// Have someone test our code - play our game!
+
+function myFunction () {
+  console.log('We\'ll add our algorithm here!');
+};
+
